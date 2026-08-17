efc4da786aaab07192c792a772ae9c48a5bc6b3c
Pranil Veer <veerpranil@gmail.com>
2026-08-18 00:42:54 +0530

    add 404 handler middleware

---
+++ server/src/middleware/notFound.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+const notFound = (_req: Request, res: Response): void => {
+  res.status(404).json({
+    success: false,
+    error: "Route not found",
+  });
+};
+
+export default notFound;
