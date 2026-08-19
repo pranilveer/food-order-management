45d05fa5e09445d9c3bf05649078575e93b4e844
Pranil Veer <veerpranil@gmail.com>
2026-08-20 02:24:20 +0530

    chore: add hotnspicy.vercel.app to CORS origins

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,6 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     "https://food-order-assign.vercel.app",
+    "https://hotnspicy.vercel.app",
     "http://localhost:3000",
   ],
   methods: ["GET", "POST", "PATCH", "OPTIONS"],
